894b5c7574c69a7cfb24eca9784b8b863f2487b4
Ernest Derry <ernest.derry@travisperkins.co.uk>
2025-04-06 12:02:20 +0100

    day4a - initial setup and horizontal xmas test

---
+++ app/src/main/java/advent/of/code/Day4a.java
@@ -0,0 +1,38 @@
+package advent.of.code;
+
+import java.util.List;
+
+public class Day4a extends Solution {
+
+    Day4a() {
+        super.day = 0;
+        super.part = "a";
+    }
+
+    String solve(List<String> puzzleInput) {
+
+        char[][] wordSearch = puzzleInput.stream()
+            .map(String::toCharArray)
+            .toArray(char[][]::new);
+
+        int noRows = wordSearch.length;
+        int noColumns = wordSearch[0].length;
+        int found = 0;
+
+        for (int row = 0; row < noRows; row++) {
+            for (int col = 0; col < noColumns; col++) {
+                if (wordSearch[row][col] == 'X') {
+                    if (wordSearch[row][col + 1] == 'M') {
+                        if (wordSearch[row][col + 2] == 'A') {
+                            if (wordSearch[row][col + 3] == 'S') {
+                                found += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return String.valueOf(found);
+    }
+}
---
+++ app/src/test/java/advent/of/code/Day4aTest.java
@@ -0,0 +1,55 @@
+package advent.of.code;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Day4aTest {
+
+    Day4a day4a;
+
+    @BeforeEach
+    public void setup() {
+        day4a = new Day4a();
+    }
+
+    @Test
+    public void horizontalXmas() {
+        List<String> puzzleInput = List.of(
+            "..XMAS",
+            "XMAS..",
+            "......",
+            ".XMAS."    
+        );
+        assertEquals("3", day4a.solve(puzzleInput));
+    }
+
+    // @Test
+    public void examplePuzzleInput() throws IOException {
+        List<String> puzzleInput = readPuzzleInputFromFile("dayX_example.txt"); 
+        assertEquals("", day4a.solve(puzzleInput));
+    }
+
+    // @Test
+    public void actualPuzzleInput() throws IOException {
+        List<String> puzzleInput = readPuzzleInputFromFile("dayX.txt"); 
+        assertEquals("", day4a.solve(puzzleInput));
+    }
+
+    private List<String> readPuzzleInputFromFile(String filename) throws FileNotFoundException, IOException {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename)) { 
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource file not found!");
+            }
+            return new BufferedReader(new InputStreamReader(inputStream)).lines().toList(); 
+        }
+    }
+}
